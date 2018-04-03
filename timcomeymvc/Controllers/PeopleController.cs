ab273ce25b0c19176cf9eaa6ba67aa2a3701a188
mk <cody.long.cl21@gmail.com>
2018-04-03 15:50:00 -0700

    added people list

---
+++ timcomeymvc/Controllers/PeopleController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace timcomeymvc.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ListPeople()
+        {
+            List<PersonModel> people = new List> PersonModel > ();
+
+            people.Add(new PersonModel { FirstName= "Stephanie", LastName="Spears", Age = 29,});
+            people.Add(new PersonModel { FirstName = "Matthew", LastName = "O'leary", Age = 34,});
+            people.Add(new PersonModel { FirstName = "Cody", LastName = "Long", Age = 25,});
+
+            return View(people);
+        }
+    }
+}
--- timcomeymvc/Models/PersonModel.cs
+++ timcomeymvc/Models/PersonModel.cs
@@ -7,5 +7,12 @@ namespace timcomeymvc.Models
 {
     public class PersonModel
     {
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+
+        public int Age { get; set; } = 0;
+
+        public bool IsAlive { get; set; } = true;
     }
 }
